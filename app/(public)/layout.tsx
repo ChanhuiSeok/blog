80b4130a11efa1869949b04696ead53cfa7c3140
Chanhui <chanhuicom@gmail.com>
2026-02-13 23:01:41 +0900

    fix: max-width 수정

--- app/(public)/layout.tsx
+++ app/(public)/layout.tsx
@@ -7,7 +7,7 @@ export default function PublicLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <div className="mx-auto max-w-3xl px-4 sm:px-6">
+    <div className="mx-auto max-w-4xl px-4 sm:px-6">
       <Header />
       <main className="py-12">{children}</main>
       <Footer />
